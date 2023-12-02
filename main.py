a733de2cf9443e1891d17c96f1bd3a6991c574ac
zakir130298 <zakir130298@gmail.com>
2023-12-02 13:11:16 +0500

    2

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
+import logging
+
+logging.basicConfig(filename='errors.log', level=logging.ERROR)
+
 def plus_numbers(a: float, b: float) -> dict:
-    result = a + b
-    return {"operation": "plus", "result": result}
+    try:
+        result = a + b
+        return {"operation": "plus", "result": result}
+    except Exception as e:
+        logging.error(f"Error in plus_numbers: {e}")
+        return {"error": f"Error in plus_numbers: {e}"}
+
 def minus_numbers(a: float, b: float) -> dict:
-    result = a - b
-    return {"operation": "minus", "result": result}
+    try:
+        result = a - b
+        return {"operation": "minus", "result": result}
+    except Exception as e:
+        logging.error(f"Error in minus_numbers: {e}")
+        return {"error": f"Error in minus_numbers: {e}"}
+
 def umnoj_numbers(a: float, b: float) -> dict:
-    result = a * b
-    return {"operation": "umnoj", "result": result}
+    try:
+        result = a * b
+        return {"operation": "umnoj", "result": result}
+    except Exception as e:
+        logging.error(f"Error in umnoj_numbers: {e}")
+        return {"error": f"Error in umnoj_numbers: {e}"}
 
 num1 = 10.5
-num2 = 5.2
+num2 = "5.2"
 
-plus_numbers = plus_numbers(num1, num2)
-print(plus_numbers)
+plus_result = plus_numbers(num1, num2)
+print(plus_result)
 
 minus_result = minus_numbers(num1, num2)
 print(minus_result)
 
-umnoj_numbers = umnoj_numbers(num1, num2)
-print(umnoj_numbers)
-
+umnoj_result = umnoj_numbers(num1, num2)
+print(umnoj_result)
